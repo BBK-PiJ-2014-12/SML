48831c0289918f05fbc2c2f47c39cfdcdb997275
BBK-PiJ-2014-12 <zsolt.balvanyos@gmail.com>
2015-01-31 17:56:24 +0000

    Bnz & Out done.

--- BnzInstruction.java
+++ BnzInstruction.java
@@ -1,5 +1,27 @@
 package sml;
 
 public class BnzInstruction extends Instruction {
-
+	private String L2;
+	private int register;
+	
+	public BnzInstruction(String l, String op) {
+		super(l, op); 
+	}
+	
+	public BnzInstruction(String label, int register, String L2) {
+		this(label, "bnz");
+		this.register = register;
+		this.L2 = L2;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + " if register " + register + " is not zero then execute " + L2;
+	}
+	
+	public void execute(Machine m) {
+		if(m.getRegisters().getRegister(register) != 0) {
+			m.setPc(m.getLabels().indexOf(L2));
+		}
+	}
 }
